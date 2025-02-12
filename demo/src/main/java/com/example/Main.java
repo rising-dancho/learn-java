3a3b554376929877bb8fd0334b665ccda02fe0ed
adfinem <zenlaws02@gmail.com>
2025-02-12 18:51:45 +0800

    feat: learning java

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,7 +1,19 @@
 package com.example;
 
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // primitive types
+        // int age = 30;
+        // age = 35;
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
+        // sout -> Tab : snippet for System.out.println()
+
+        // Constructor: reference type
+        Date now = new Date();
+        System.out.println(now);
     }
 }
